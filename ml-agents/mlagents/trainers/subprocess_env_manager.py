46060dadc73114907c6372abe6f947303b8b9e66
Jason Bowman <jasonb@unity3d.com>
2021-08-31 17:50:59 -0700

    Add support for demo_path filtering, update names, and update class dir

--- ml-agents/mlagents/trainers/subprocess_env_manager.py
+++ ml-agents/mlagents/trainers/subprocess_env_manager.py
@@ -37,7 +37,7 @@ from mlagents_envs.side_channel.stats_side_channel import (
     EnvironmentStats,
     StatsSideChannel,
 )
-from mlagents.training_analytics_side_channel import TrainingAnalyticsSideChannel
+from mlagents.telemetry.training_analytics_side_channel import TrainingAnalyticsSideChannel
 from mlagents_envs.side_channel.side_channel import SideChannel
 
 
